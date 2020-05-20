ebd421324c7e81dc121acae9b12b0cde27670215
Roger Oriol <rogi23696@gmail.com>
2020-05-20 22:36:30 +0200

    api gateway

--- balance-service/get-dependencies.sh
+++ balance-service/get-dependencies.sh
@@ -1,4 +1,3 @@
 go get "gopkg.in/mgo.v2"
-go get "gopkg.in/mgo.v2/bson"
 go get "github.com/gin-gonic/gin"
 go get "github.com/dgrijalva/jwt-go"
--- balance-service/src/controllers/balance.go
+++ balance-service/src/controllers/balance.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"log"
+
 	"../common"
 	"../models"
 	"github.com/gin-gonic/gin"
@@ -19,12 +21,15 @@ func GetBalance(ctx *gin.Context) {
 		ctx.JSON(403, gin.H{
 			"message": "Invalid authentication",
 		})
+		return
 	}
 	var balance, err = models.GetBalance(username)
 	if err != nil {
+		log.Fatal(err)
 		ctx.JSON(500, gin.H{
 			"message": err,
 		})
+		return
 	}
 	ctx.JSON(200, gin.H{
 		"balance": balance,
